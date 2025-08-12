5ee16f4c611ae0e3fa56dda74b86a1c4f4b15c4f
Leighton Grant <oneal.grant@outlook.com>
2025-08-12 17:02:02 +0100

    Refactor main.tsx to explicitly type rootElement as HTMLDivElement

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import 'aos/dist/aos.css'
 import './index.css'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { StrictMode } from 'react'
 import AosProvider from './components/aos/AosProvider'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const rootElement = document.getElementById('root') as HTMLDivElement
+const root = ReactDOM.createRoot(rootElement)
 root.render(
 	<StrictMode>
 		<AosProvider>
